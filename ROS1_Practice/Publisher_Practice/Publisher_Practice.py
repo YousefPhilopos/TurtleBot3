255f92bf7ddd4baf713cc8c14bc4558b36ea6a8f
YousefPhilopos <120761649+YousefPhilopos@users.noreply.github.com>
2022-12-17 21:18:08 +0400

    Update Publisher_Practice.py

--- ROS1_Practice/Publisher_Practice/Publisher_Practice.py
+++ ROS1_Practice/Publisher_Practice/Publisher_Practice.py
@@ -1,31 +1,30 @@
-import rospy
 #Allows use of ROS functionalities such as interactions with topics
+import rospy
 
-from geometry_msgs.msg import Twist
 #Imports the class twist which is the class used to command the turtle in the turtle sim node.
+from geometry_msgs.msg import Twist
 
 if __name__ == '__main__':
-    rospy.init_node("draw_circle")
     #names and initialises the node
-    rospy.loginfo("Node has been started.")
+    rospy.init_node("draw_circle")
     #Outputs "Node has been started." to the console
-
-    pub = rospy.Publisher("/turtle1/cmd_vel",Twist, queue_size = 10)
+    rospy.loginfo("Node has been started.") 
+    
     # establishes pub as the Publiser rospy method, and outline the topic, class and queue size
-
-    rate = rospy.Rate(2)
+    pub = rospy.Publisher("/turtle1/cmd_vel",Twist, queue_size = 10)
+    
     # establishes the rate of 2 hertz and assigns it to variable rate
+    rate = rospy.Rate(2)
 
+    #Creates a while loop that is active while this node is running
     while not rospy.is_shutdown():
-        #Creates a while loop that is active while this node is running
-        
-        msg = Twist()
+       
         # assigns Twist class to "msg"
+        msg = Twist()
+        #Uses twist methods to set the desired linear and angular velocity of the turtle
         msg.linear.x = 2.0
         msg.angular.z = 1.0
-        #Uses twist methods to set the desired linear and angular velocity of the turtle
-        pub.publish(msg)
         # uses the publish method to send the msg twist class to the /turtle1/cmd_vel topic.
-        rate.sleep
+        pub.publish(msg)
         #pauses the loop for 2Hz
-        
+        rate.sleep
